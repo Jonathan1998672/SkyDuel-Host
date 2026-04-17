4285a21e08817e3578cc7bfe179bec48134c7393
Jonathan1998672 <jonathangarcia35540@gmail.com>
2026-04-16 20:39:35 -0600

    Add: db en package

--- server.js
+++ server.js
@@ -21,3 +21,20 @@ const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
     console.log(`Servidor Sky Duel corriendo en puerto ${PORT}`);
 });
+
+const mysql = require('mysql2');
+
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'u649554040_Pilot',
+    password: 'DszUQcpjYKitDs9?',
+    database: 'u649554040_SkyDuel'
+});
+
+db.connect((err) => {
+    if (err) {
+        console.error('Error conectando a MySQL:', err);
+        return;
+    }
+    console.log('Servidor Node.js conectado a la base de datos MySQL');
+});
